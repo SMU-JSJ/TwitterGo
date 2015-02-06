83759e7a56cf0dd51dcce8dad3cdc6c30bcd0e94
Jordan Kayse <jkayse@smu.edu>
2015-02-05 21:46:43 -0600

    added team member names

--- TwitterGo/TweetTableVC.m
+++ TwitterGo/TweetTableVC.m
@@ -1,4 +1,4 @@
-//
+//  Jordan Kayse, Jessica Yeh, and Story Zanetti
 //  TweetTableVC.m
 //  TwitterGo
 //
@@ -17,8 +17,8 @@
 
 @property (strong, nonatomic) TweetModel* tweetModel;
 
-@property (weak, nonatomic) IBOutlet UIBarButtonItem *currentTrend;
-@property (strong, nonatomic) IBOutlet UITableView *tableView;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem* currentTrend;
+@property (strong, nonatomic) IBOutlet UITableView* tableView;
 
 @property (strong, nonatomic) NSString* type;
 @property (strong, nonatomic) NSTimer* timer;
@@ -58,7 +58,7 @@
     NSInteger selectedSegment = sender.selectedSegmentIndex;
     
     // Change type to match
-    if (selectedSegment == 0 ) {
+    if (selectedSegment == 0) {
         self.type = @"popular";
     } else if (selectedSegment == 1) {
         self.type = @"recent";
@@ -353,41 +353,6 @@
         return cell;
     }
 }
-
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
 
 #pragma mark - Navigation
 
